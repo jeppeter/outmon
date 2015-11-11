2ca9f0fc653e8b463583b10e5b1700a086944a9c
jeppeter <jeppeter@gmail.com>
2015-11-11 20:06:41 +0800

    to add x86make.bat and x64make.bat

--- x64make.bat
+++ x64make.bat
@@ -1,5 +1,6 @@
 set RELEASE_ARCH=x64
 set VC_SETVAR_ARCH=amd64
 set _IsNativeEnvironment=true
+cscript verify_vs.vbs
 cscript runmake.vbs makefile.nmake clean
 cscript runmake.vbs makefile.nmake all
--- x86make.bat
+++ x86make.bat
@@ -1,4 +1,5 @@
 set RELEASE_ARCH=x86
 set VC_SETVAR_ARCH=amd64_x86
+cscript verify_vs.vbs
 cscript runmake.vbs makefile.nmake clean
 cscript runmake.vbs makefile.nmake all
